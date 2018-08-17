6e4a54b9e79b19723af1e9b41a21cccd11192e50
RodrigoSeverino <rodrigoseverino.103@gmail.com>
2018-08-17 00:23:35 -0300

    server.php

--- ProyectoIntegrador/resources/views/includes/header.blade.php
+++ ProyectoIntegrador/resources/views/includes/header.blade.php
@@ -1,8 +1,8 @@
 <nav>
-  <a href="/viewss/home.php"><img src="images/logo.PNG" id="logo" alt="logo"></a>
+  <a href="/home"><img src="images/logo.PNG" id="logo" alt="logo"></a>
   <ul>
 
-    <li><a href="./viewss/carrito.blade.php">Carrito</a></li>
+    <li><a href="/carrito">Carrito</a></li>
     <li><a href="/viewss/contacto.php">Contáctanos</a></li>
     <li><a href="/viewss/PreguntasFrecuentes.php">Preguntas Frecuentes</a></li>
   </ul>
--- ProyectoIntegrador/resources/views/viewss/carrito.blade.php
+++ ProyectoIntegrador/resources/views/viewss/carrito.blade.php
@@ -8,7 +8,7 @@
   <title>Carrito</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=devise-width, initial-scale=1">
-  <link rel="stylesheet"  href="/public/css/carrito.css">
+  <link rel="stylesheet"  href="/css/carrito.css">
   <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
 </head>
 <body>
