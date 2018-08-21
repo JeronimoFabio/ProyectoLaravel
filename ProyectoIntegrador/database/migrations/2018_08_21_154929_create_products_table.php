4944ba438c5f0b9f66d66298cb8c8d175a4f217f
JeronimoFabio <jeronimo.fabio@hotmail.com>
2018-08-21 13:53:25 -0300

    todavia no puede logear

--- ProyectoIntegrador/database/migrations/2018_08_21_154929_create_products_table.php
+++ ProyectoIntegrador/database/migrations/2018_08_21_154929_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
+            $table->string('name');
             $table->integer('precio');
             $table->integer('stock');
             $table->timestamps();
