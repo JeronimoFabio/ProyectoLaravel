882fb80109850e2af04afad24fc9400d0c77edca
RodrigoSeverino <rodrigoseverino.103@gmail.com>
2018-08-24 16:12:16 -0300

    algunas correcciones

--- Proyecto.Laravel/database/migrations/2018_08_24_161330_create_products_table.php
+++ Proyecto.Laravel/database/migrations/2018_08_24_161330_create_products_table.php
@@ -6,27 +6,22 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            //***// */
             $table->string('name');
-            $table->integer('price');
-            $table->integer('stock');
+            $table->text('description')->charset('utf8');
+            $table->string('photo');
+            $table->float('price')->unsigned();
+            $table->integer('stock')->unsigned()->default(0);
+            $table->integer('categories_id')->unsigned()->default(1);
+            //***// */
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('products');
